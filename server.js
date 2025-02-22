23f6ab0795f72e79724809f5725381bb260d9ba8
sayzs1@yeah.net <sayzs1@yeah.net>
2025-02-22 10:42:09 -0800

    Update package.json and GitHub workflow for deployment optimization

---
+++ server.js
@@ -0,0 +1,25 @@
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const port = process.env.PORT || 3000
+
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true)
+      await handle(req, res, parsedUrl)
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err)
+      res.statusCode = 500
+      res.end('internal server error')
+    }
+  })
+  .listen(port, () => {
+    console.log(`> Ready on port ${port}`)
+  })
+}) 
